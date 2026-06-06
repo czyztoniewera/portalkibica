c4ce2ec548bff9a2ef550029c78b588cc93cbc2d
Weronika <czyzweronika2806@gmail.com>
2026-06-06 14:58:46 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortalKibica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
