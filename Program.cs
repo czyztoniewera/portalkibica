708b0144aebb32a39d3d41502d0f3e24e92d43d7
Weronika <czyzweronika2806@gmail.com>
2026-06-14 12:19:15 +0200

    Seed danych, licznik znaków w formularzu News

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PortalKibica.Data;
+using PortalKibica.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -60,6 +61,43 @@ using (var scope = app.Services.CreateScope())
             EmailConfirmed = true
         };
         await userManager.CreateAsync(user, password);
+    }
+}
+
+// Seed przykladowych danych (tylko jesli tabele sa puste)
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    if (!context.Players.Any())
+    {
+        context.Players.AddRange(
+            new Player { Name = "Kamil Broda", Position = "Bramkarz", Number = 1, Description = "DoŚwiadczony bramkarz, kapitan zespoŁu." },
+            new Player { Name = "Anton Chichkan", Position = "Bramkarz", Number = 31, Description = "Drugi bramkarz w sezonie 2025/2026." }
+        );
+        context.SaveChanges();
+    }
+
+    if (!context.News.Any())
+    {
+        context.News.AddRange(
+            new News
+            {
+                Title = "Wisła wygrywa derby Krakowa",
+                Content = "Dnia 7 listopada 2021 roku zespół Wisły Kraków odniósł bardzo ważne zwycięstwo w derbach miasta Krakowa. Kibice na stadionie stworzyli niesamowitą atmosferę przez całe 90 minut. Trener pochwalił zaangazowanie całej drużyny i podkreslił znaczenie tego zwycięstwa dla morali zespołu przed kolejnymi spotkaniami w lidze.",
+                PublishDate = new DateTime(2026, 6, 1, 18, 0, 0)
+            }
+        );
+        context.SaveChanges();
+    }
+
+    if (!context.Matches.Any())
+    {
+        context.Matches.AddRange(
+            new Match { Opponent = "Widzew Łódz", MatchDate = new DateTime(2026, 9, 5, 20, 0, 0), Stadium = "Stadion Miejski im. Henryka Reymana" },
+            new Match { Opponent = "Zagłębie Lublin", MatchDate = new DateTime(2026, 8, 2, 18, 0, 0), Stadium = "Stadion KGHM Zagłębie Arena" }
+        );
+        context.SaveChanges();
     }
 }
 
